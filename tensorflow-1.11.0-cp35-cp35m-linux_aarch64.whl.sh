031b597787e038078539a57b70a20424ab22995a
PINTO0309 <rmsdh122@yahoo.co.jp>
2019-07-16 00:26:37 +0900

    Save old wheel file

--- tensorflow-1.11.0-cp35-cp35m-linux_aarch64.whl.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-curl -sc /tmp/cookie "https://drive.google.com/uc?export=download&id=1DgzRmgjn2EB-cANflk2jnj9_auiHDl7A" > /dev/null
-CODE="$(awk '/_warning_/ {print $NF}' /tmp/cookie)"
-curl -Lb /tmp/cookie "https://drive.google.com/uc?export=download&confirm=${CODE}&id=1DgzRmgjn2EB-cANflk2jnj9_auiHDl7A" -o tensorflow-1.11.0-cp35-cp35m-linux_aarch64.whl
-sudo pip3 uninstall tensorflow
-sudo -H pip3 install tensorflow-1.11.0-cp35-cp35m-linux_aarch64.whl
