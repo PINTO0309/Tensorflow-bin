733367c72541651571f05f2453d9d05da5a0a992
PINTO0309 <rmsdh122@yahoo.co.jp>
2019-10-25 22:46:48 +0900

    update

--- previous_versions/download_tensorflow-2.0.0-cp27-cp27mu-linux_armv7l.sh
+++ previous_versions/download_tensorflow-2.0.0-cp27-cp27mu-linux_armv7l.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-curl -sc /tmp/cookie "https://drive.google.com/uc?export=download&id=13jEjusLmfaI0yph1LHfMF6hbocNuZu2c" > /dev/null
+curl -sc /tmp/cookie "https://drive.google.com/uc?export=download&id=1C5z-yr4Cf64_CvyRKUM31jhD89T_LA0E" > /dev/null
 CODE="$(awk '/_warning_/ {print $NF}' /tmp/cookie)"
-curl -Lb /tmp/cookie "https://drive.google.com/uc?export=download&confirm=${CODE}&id=13jEjusLmfaI0yph1LHfMF6hbocNuZu2c" -o tensorflow-2.0.0-cp27-cp27mu-linux_armv7l.whl
+curl -Lb /tmp/cookie "https://drive.google.com/uc?export=download&confirm=${CODE}&id=1C5z-yr4Cf64_CvyRKUM31jhD89T_LA0E" -o tensorflow-2.0.0-cp27-cp27mu-linux_armv7l.whl
 echo Download finished.
--- previous_versions/download_tensorflow-2.0.0-cp37-cp37m-linux_aarch64.sh
+++ previous_versions/download_tensorflow-2.0.0-cp37-cp37m-linux_aarch64.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-curl -sc /tmp/cookie "https://drive.google.com/uc?export=download&id=1VMQd4d3AmaGSU1ioTn5lkOlurI8FOGO7" > /dev/null
+curl -sc /tmp/cookie "https://drive.google.com/uc?export=download&id=1M7vQ4dV73SUcnoonyF6qjeYhLIYBTnGv" > /dev/null
 CODE="$(awk '/_warning_/ {print $NF}' /tmp/cookie)"
-curl -Lb /tmp/cookie "https://drive.google.com/uc?export=download&confirm=${CODE}&id=1VMQd4d3AmaGSU1ioTn5lkOlurI8FOGO7" -o tensorflow-2.0.0-cp37-cp37m-linux_aarch64.whl
+curl -Lb /tmp/cookie "https://drive.google.com/uc?export=download&confirm=${CODE}&id=1M7vQ4dV73SUcnoonyF6qjeYhLIYBTnGv" -o tensorflow-2.0.0-cp37-cp37m-linux_aarch64.whl
 echo Download finished.
