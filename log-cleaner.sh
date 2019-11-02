d5351052e8c00d34e3b368c507f5def78ede3238
PINTO0309 <rmsdh122@yahoo.co.jp>
2019-11-02 21:28:04 +0900

    update

--- log-cleaner.sh
+++ log-cleaner.sh
@@ -7,5 +7,6 @@ TARGETS=(
 target=$(printf " %s" "${TARGETS[@]}")
 target=${target:1}
 
+sudo rm -rf .git/refs/original
 git filter-branch --index-filter "git rm -r --cached --ignore-unmatch ${target}" -- --all
 
