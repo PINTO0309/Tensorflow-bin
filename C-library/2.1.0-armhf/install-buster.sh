bddfcbad711eb2f18780bdba9048e856e69721a3
Katsuya Hyodo <rmsdh122@yahoo.co.jp>
2020-01-17 06:30:37 +0900

    update

--- C-library/2.1.0-armhf/install-buster.sh
+++ C-library/2.1.0-armhf/install-buster.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-curl -sc /tmp/cookie "https://drive.google.com/uc?export=download&id=1l9t15EBXlM1dUmnRH17F1ubh4JjR7MG4" > /dev/null
+curl -sc /tmp/cookie "https://drive.google.com/uc?export=download&id=1FZBc1VvRCnWv9deSWiG5dQCHs0fyUBx0" > /dev/null
 CODE="$(awk '/_warning_/ {print $NF}' /tmp/cookie)"
-curl -Lb /tmp/cookie "https://drive.google.com/uc?export=download&confirm=${CODE}&id=1l9t15EBXlM1dUmnRH17F1ubh4JjR7MG4" -o libtensorflow.tar.gz
+curl -Lb /tmp/cookie "https://drive.google.com/uc?export=download&confirm=${CODE}&id=1FZBc1VvRCnWv9deSWiG5dQCHs0fyUBx0" -o libtensorflow.tar.gz
 tar -C /usr/local -xzf libtensorflow.tar.gz
 rm libtensorflow.tar.gz
 sudo ldconfig
