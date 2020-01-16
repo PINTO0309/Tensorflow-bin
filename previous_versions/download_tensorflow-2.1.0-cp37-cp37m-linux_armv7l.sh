cc00ab2d6086bd3a63f02779ed47ede3579cc4c9
Katsuya Hyodo <rmsdh122@yahoo.co.jp>
2020-01-16 22:41:49 +0900

    update

--- previous_versions/download_tensorflow-2.1.0-cp37-cp37m-linux_armv7l.sh
+++ previous_versions/download_tensorflow-2.1.0-cp37-cp37m-linux_armv7l.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 
-curl -sc /tmp/cookie "https://drive.google.com/uc?export=download&id=1twAdG4G5G7PswjL55D-Ei2Mkh21JBA4M" > /dev/null
+curl -sc /tmp/cookie "https://drive.google.com/uc?export=download&id=1nAIYS-f-p-gmj1MzgZ9R3imtmrvlkna0" > /dev/null
 CODE="$(awk '/_warning_/ {print $NF}' /tmp/cookie)"
-curl -Lb /tmp/cookie "https://drive.google.com/uc?export=download&confirm=${CODE}&id=1twAdG4G5G7PswjL55D-Ei2Mkh21JBA4M" -o tensorflow-2.1.0-cp37-cp37m-linux_armv7l.whl
+curl -Lb /tmp/cookie "https://drive.google.com/uc?export=download&confirm=${CODE}&id=1nAIYS-f-p-gmj1MzgZ9R3imtmrvlkna0" -o tensorflow-2.1.0-cp37-cp37m-linux_armv7l.whl
 echo Download finished.
-
-https://drive.google.com/open?id=
