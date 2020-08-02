5eac432ee9ca1777288e97b00574808f1fbc0ae0
Katsuya Hyodo <rmsdh122@yahoo.co.jp>
2020-08-02 10:21:24 +0900

    Ubuntu 20.04 aarch64, FlexDelegate, XNNPACK enabled

---
+++ tensorflow-2.3.0-cp38-cp38m-linux_aarch64_download.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+curl -sc /tmp/cookie "https://drive.google.com/uc?export=download&id=1EbHTYyBPhpirEjt4gGBynPteK5He7ama" > /dev/null
+CODE="$(awk '/_warning_/ {print $NF}' /tmp/cookie)"
+curl -Lb /tmp/cookie "https://drive.google.com/uc?export=download&confirm=${CODE}&id=1EbHTYyBPhpirEjt4gGBynPteK5He7ama" -o tensorflow-2.3.0-cp38-cp38m-linux_aarch64.whl
+echo Download finished.
