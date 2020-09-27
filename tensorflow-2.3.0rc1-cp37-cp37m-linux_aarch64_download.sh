93775793ba365034df6846268ccfa4631b142dce
Katsuya Hyodo <rmsdh122@yahoo.co.jp>
2020-09-27 23:35:44 +0900

    cleaning

--- tensorflow-2.3.0rc1-cp37-cp37m-linux_aarch64_download.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-curl -sc /tmp/cookie "https://drive.google.com/uc?export=download&id=15nVB11d_PXseIECTqKCMtau-bgR3ATfb" > /dev/null
-CODE="$(awk '/_warning_/ {print $NF}' /tmp/cookie)"
-curl -Lb /tmp/cookie "https://drive.google.com/uc?export=download&confirm=${CODE}&id=15nVB11d_PXseIECTqKCMtau-bgR3ATfb" -o tensorflow-2.3.0rc1-cp37-cp37m-linux_aarch64.whl
-echo Download finished.
--- tensorflow-2.3.0rc1-cp37-none-linux_armv7l_download.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-curl -sc /tmp/cookie "https://drive.google.com/uc?export=download&id=1e1Aa9fuNUiY0SyDBxrfNukPIxZTEiiz9" > /dev/null
-CODE="$(awk '/_warning_/ {print $NF}' /tmp/cookie)"
-curl -Lb /tmp/cookie "https://drive.google.com/uc?export=download&confirm=${CODE}&id=1e1Aa9fuNUiY0SyDBxrfNukPIxZTEiiz9" -o tensorflow-2.3.0rc1-cp37-none-linux_armv7l.whl
-echo Download finished.
