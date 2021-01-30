4938a6b6922b96e4cc9a3f1c6f15d84fd5f85c2b
Katsuya Hyodo <rmsdh122@yahoo.co.jp>
2021-01-30 16:51:21 +0900

    Support for custom operations in MediaPipe

--- tensorflow-2.4.0-cp38-none-linux_aarch64_download.sh
+++ tensorflow-2.4.0-cp38-none-linux_aarch64_download.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-curl -sc /tmp/cookie "https://drive.google.com/uc?export=download&id=1Od_UAUYP4kuOHI4D6sopiWrwbVCqZxyZ" > /dev/null
+curl -sc /tmp/cookie "https://drive.google.com/uc?export=download&id=11GVD2mlknhhrlNQignGZkdGhIsKxeOje" > /dev/null
 CODE="$(awk '/_warning_/ {print $NF}' /tmp/cookie)"
-curl -Lb /tmp/cookie "https://drive.google.com/uc?export=download&confirm=${CODE}&id=1Od_UAUYP4kuOHI4D6sopiWrwbVCqZxyZ" -o tensorflow-2.4.0-cp38-none-linux_aarch64.whl
+curl -Lb /tmp/cookie "https://drive.google.com/uc?export=download&confirm=${CODE}&id=11GVD2mlknhhrlNQignGZkdGhIsKxeOje" -o tensorflow-2.4.0-cp38-none-linux_aarch64.whl
 echo Download finished.
