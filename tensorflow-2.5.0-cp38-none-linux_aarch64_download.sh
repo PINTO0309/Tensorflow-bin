cd6652e5f3d90139e97d0d192dcc70c295bb88f2
Katsuya Hyodo <rmsdh122@yahoo.co.jp>
2021-07-30 00:31:43 +0900

    Update tensorflow-2.5.0-cp38-none-linux_aarch64_download.sh

--- tensorflow-2.5.0-cp38-none-linux_aarch64_download.sh
+++ tensorflow-2.5.0-cp38-none-linux_aarch64_download.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-curl -sc /tmp/cookie "https://drive.google.com/uc?export=download&id=1dLS8Faq0wuipbfBuzNgTl4Uv9jBj8K7c" > /dev/null
+curl -sc /tmp/cookie "https://drive.google.com/uc?export=download&id=1yL0WOKA339ETUdCbjkK7O3cLYJneo1F5" > /dev/null
 CODE="$(awk '/_warning_/ {print $NF}' /tmp/cookie)"
-curl -Lb /tmp/cookie "https://drive.google.com/uc?export=download&confirm=${CODE}&id=1dLS8Faq0wuipbfBuzNgTl4Uv9jBj8K7c" -o tensorflow-2.5.0-cp38-none-linux_aarch64.whl
+curl -Lb /tmp/cookie "https://drive.google.com/uc?export=download&confirm=${CODE}&id=1yL0WOKA339ETUdCbjkK7O3cLYJneo1F5" -o tensorflow-2.5.0-cp38-none-linux_aarch64.whl
 echo Download finished.
