6f7cedd4fc3b7d52bd31f7767d6a61256ccf04f4
PINTO0309 <rmsdh122@yahoo.co.jp>
2021-08-12 23:18:04 +0900

    2.6.0

--- tensorflow-2.5.0-cp37-none-linux_aarch64_numpy1195_download.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-curl -sc /tmp/cookie "https://drive.google.com/uc?export=download&id=1o__C1dBp2-8D0k5Ggj6aUU2alG6J_u55" > /dev/null
-CODE="$(awk '/_warning_/ {print $NF}' /tmp/cookie)"
-curl -Lb /tmp/cookie "https://drive.google.com/uc?export=download&confirm=${CODE}&id=1o__C1dBp2-8D0k5Ggj6aUU2alG6J_u55" -o tensorflow-2.5.0-cp37-none-linux_aarch64.whl
-echo Download finished.
