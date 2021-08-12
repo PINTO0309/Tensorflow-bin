9ef428a9f1d4ddfb158ecf48955700ab52dbb7ae
Katsuya Hyodo <rmsdh122@yahoo.co.jp>
2021-08-12 23:29:54 +0900

    Update tensorflow-2.6.0-cp39-none-linux_aarch64_numpy1211_download.sh

--- tensorflow-2.6.0-cp39-none-linux_aarch64_numpy1211_download.sh
+++ tensorflow-2.6.0-cp39-none-linux_aarch64_numpy1211_download.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-curl -sc /tmp/cookie "https://drive.google.com/uc?export=download&id=1hzv5FwsI5K-yywxfZWN5XzJRA2VoXoE5" > /dev/null
+curl -sc /tmp/cookie "https://drive.google.com/uc?export=download&id=1kVXgUttQ-BoC9RplRFborVmqmmpGly0j" > /dev/null
 CODE="$(awk '/_warning_/ {print $NF}' /tmp/cookie)"
-curl -Lb /tmp/cookie "https://drive.google.com/uc?export=download&confirm=${CODE}&id=1hzv5FwsI5K-yywxfZWN5XzJRA2VoXoE5" -o tensorflow-2.6.0-cp39-none-linux_aarch64.whl
+curl -Lb /tmp/cookie "https://drive.google.com/uc?export=download&confirm=${CODE}&id=1kVXgUttQ-BoC9RplRFborVmqmmpGly0j" -o tensorflow-2.6.0-cp39-none-linux_aarch64.whl
 echo Download finished.
