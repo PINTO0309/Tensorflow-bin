97428d90a1c0af36059e7a7f95d51e9fb1ebb145
PINTO0309 <rmsdh122@yahoo.co.jp>
2021-08-12 21:23:01 +0900

    2.6.0

--- tensorflow-2.6.0rc1-cp37-none-linux_aarch64_numpy1195_download.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-curl -sc /tmp/cookie "https://drive.google.com/uc?export=download&id=1EShQuBJKDC0blvm-y_-y5e9DN9k2Yy5a" > /dev/null
-CODE="$(awk '/_warning_/ {print $NF}' /tmp/cookie)"
-curl -Lb /tmp/cookie "https://drive.google.com/uc?export=download&confirm=${CODE}&id=1EShQuBJKDC0blvm-y_-y5e9DN9k2Yy5a" -o tensorflow-2.6.0rc1-cp37-none-linux_aarch64.whl
-echo Download finished.
--- tensorflow-2.6.0rc1-cp38-none-linux_aarch64_numpy1195_download.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-curl -sc /tmp/cookie "https://drive.google.com/uc?export=download&id=1iM4vD2lpuaARCv4zvEzheI0XABiys6Nq" > /dev/null
-CODE="$(awk '/_warning_/ {print $NF}' /tmp/cookie)"
-curl -Lb /tmp/cookie "https://drive.google.com/uc?export=download&confirm=${CODE}&id=1iM4vD2lpuaARCv4zvEzheI0XABiys6Nq" -o tensorflow-2.6.0rc1-cp38-none-linux_aarch64.whl
-echo Download finished.
