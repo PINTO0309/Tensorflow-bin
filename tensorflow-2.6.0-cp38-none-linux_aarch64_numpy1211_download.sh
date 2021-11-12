23e0dfd101646007a9ce8441667e1fd1f5bd1d09
pinto0309 <rmsdh122@yahoo.co.jp>
2021-11-12 11:46:33 +0900

    v2.7.0

--- tensorflow-2.6.0-cp38-none-linux_aarch64_numpy1211_download.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-curl -sc /tmp/cookie "https://drive.google.com/uc?export=download&id=1hz1z0t_0kgMdInVElQJuE4NiqJi7IsZ_" > /dev/null
-CODE="$(awk '/_warning_/ {print $NF}' /tmp/cookie)"
-curl -Lb /tmp/cookie "https://drive.google.com/uc?export=download&confirm=${CODE}&id=1hz1z0t_0kgMdInVElQJuE4NiqJi7IsZ_" -o tensorflow-2.6.0-cp38-none-linux_aarch64.whl
-echo Download finished.
