ea90aad470966976febd47e0c33fa3aa470ff302
pinto0309 <rmsdh122@yahoo.co.jp>
2022-02-03 08:49:48 +0900

    armv7l delete

--- tensorflow-1.15.0-cp27-cp27mu-linux_armv7l_download.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-curl -sc /tmp/cookie "https://drive.google.com/uc?export=download&id=1jfc92gmDyt-sehIb9ygnzOSVDj7sy9oJ" > /dev/null
-CODE="$(awk '/_warning_/ {print $NF}' /tmp/cookie)"
-curl -Lb /tmp/cookie "https://drive.google.com/uc?export=download&confirm=${CODE}&id=1jfc92gmDyt-sehIb9ygnzOSVDj7sy9oJ" -o tensorflow-1.15.0-cp27-cp27mu-linux_armv7l.whl
-echo Download finished.
--- tensorflow-1.15.0-cp37-cp37m-linux_aarch64_download.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-curl -sc /tmp/cookie "https://drive.google.com/uc?export=download&id=1D9IT2WC2mrWOixsNKohxiJJIlY2IzmQQ" > /dev/null
-CODE="$(awk '/_warning_/ {print $NF}' /tmp/cookie)"
-curl -Lb /tmp/cookie "https://drive.google.com/uc?export=download&confirm=${CODE}&id=1D9IT2WC2mrWOixsNKohxiJJIlY2IzmQQ" -o tensorflow-1.15.0-cp37-cp37m-linux_aarch64.whl
-echo Download finished.
