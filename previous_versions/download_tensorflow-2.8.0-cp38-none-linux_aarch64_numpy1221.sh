7b8d75a16b4975714a21e6c0c75de5dc54cb5816
Katsuya Hyodo <rmsdh122@yahoo.co.jp>
2022-02-09 08:02:38 +0900

    Update download_tensorflow-2.8.0-cp38-none-linux_aarch64_numpy1221.sh

--- previous_versions/download_tensorflow-2.8.0-cp38-none-linux_aarch64_numpy1221.sh
+++ previous_versions/download_tensorflow-2.8.0-cp38-none-linux_aarch64_numpy1221.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
 
-curl -sc /tmp/cookie "https://drive.google.com/uc?export=download&id=1upJD-J4Z4jPu5wTx3FW_KpK7t7oAj5Ac" > /dev/null
-CODE="$(awk '/_warning_/ {print $NF}' /tmp/cookie)"
-curl -Lb /tmp/cookie "https://drive.google.com/uc?export=download&confirm=${CODE}&id=1upJD-J4Z4jPu5wTx3FW_KpK7t7oAj5Ac" -o tensorflow-2.8.0-cp38-none-linux_aarch64.whl
+curl -L https://github.com/PINTO0309/Tensorflow-bin/releases/download/v2.8.0/tensorflow-2.8.0-cp38-none-linux_aarch64.whl -o tensorflow-2.8.0-cp38-none-linux_aarch64.whl
 echo Download finished.
