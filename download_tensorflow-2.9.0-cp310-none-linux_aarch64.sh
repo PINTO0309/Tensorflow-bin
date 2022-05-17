bc25e903b9cd268eb3416bc5ea8fc852bef9f1ef
pinto0309 <rmsdh122@yahoo.co.jp>
2022-05-17 21:13:36 +0900

    v2.9.0

---
+++ download_tensorflow-2.9.0-cp310-none-linux_aarch64.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+curl -L https://github.com/PINTO0309/Tensorflow-bin/releases/download/v2.9.0/tensorflow-2.9.0-cp310-none-linux_aarch64.whl -o tensorflow-2.9.0-cp310-none-linux_aarch64.whl
+echo Download finished.
