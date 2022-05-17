c14cff4c6c72c10e0b8b7002f531bd009d7d2e50
pinto0309 <rmsdh122@yahoo.co.jp>
2022-05-17 20:14:14 +0900

    v2.9.0

--- tensorflow-2.8.0-cp39-none-linux_aarch64_numpy1221_download.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-curl -L https://github.com/PINTO0309/Tensorflow-bin/releases/download/v2.8.0/tensorflow-2.8.0-cp39-none-linux_aarch64.whl -o tensorflow-2.8.0-cp39-none-linux_aarch64.whl
-echo Download finished.
