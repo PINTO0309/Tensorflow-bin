4e7ee587c3920dc3e41bb96270e8454081dd70ab
pinto0309 <rmsdh122@yahoo.co.jp>
2022-09-07 11:49:13 +0900

    v2.10.0

---
+++ download_tensorflow-2.10.0-cp39-none-linux_aarch64.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+TFVER=2.10.0
+curl -L https://github.com/PINTO0309/Tensorflow-bin/releases/download/v${TFVER}/tensorflow-${TFVER}-cp39-none-linux_aarch64.whl -o tensorflow-${TFVER}-cp39-none-linux_aarch64.whl
+echo Download finished.
