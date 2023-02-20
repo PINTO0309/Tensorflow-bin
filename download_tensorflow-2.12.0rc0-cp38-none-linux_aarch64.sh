da9276e3fcc1d75a877fe760f429dc2a5db248cd
pinto0309 <rmsdh122@yahoo.co.jp>
2023-02-20 18:02:20 +0900

    v2.12.0rc0

---
+++ download_tensorflow-2.12.0rc0-cp38-none-linux_aarch64.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+TFVER=2.12.0rc0
+curl -L https://github.com/PINTO0309/Tensorflow-bin/releases/download/v${TFVER}/tensorflow-${TFVER}-cp38-none-linux_aarch64.whl -o tensorflow-${TFVER}-cp38-none-linux_aarch64.whl
+echo Download finished.
